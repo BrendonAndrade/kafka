145a1b0afe1067eeca5bc81a2100952f212d27f0
Brendon Andrade <brendon@ciandt.com>
2023-12-08 14:44:38 -0300

    Incluindo callback no send message

--- producer/src/main/java/com/kafka/producer/services/StringProducerService.java
+++ producer/src/main/java/com/kafka/producer/services/StringProducerService.java
@@ -1,16 +1,33 @@
 package com.kafka.producer.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("str-topic", message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("str-topic", message);
+
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                log.error("Error sending message: {}", ex.getMessage());
+                return;
+            }
+            log.info("Message sent successfully: {}", result.getProducerRecord().value());
+            log.info(
+                    "Partition {}, Offset {}",
+                    result.getRecordMetadata().partition(),
+                    result.getRecordMetadata().offset()
+            );
+        });
     }
 }
